ea87b8f9c0fb094c79ec7b0d409af56caf453219
David Coleman <77750995+MrDaveColeman@users.noreply.github.com>
2024-09-17 16:15:05 +0000

    Added UserManager class to test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-#hello team this is test file!!!!!
+# David Coleman's code
+# UserManager class to handle user-related operations
+class UserManager:
+    def __init__(self):
+        self.db = Database()
+
+    def add_user(self, user_id, name, email):
+        user = User(user_id, name, email)
+        self.db.add_user(user)
+
+    def delete_user(self, user_id):
+        self.db.delete_user(user_id)
+
+    def update_user(self, user_id, name, email):
+        self.db.update_user(user_id, name, email)
+
+    def list_users(self):
+        return self.db.get_users()
